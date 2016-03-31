4139a6d9cd176eedd85a37364d2943401de709d3
Andrey Kaipov <akaip002@fiu.edu>
2016-03-31 02:51:04 -0400

    Add mpc to parse our grammars.

--- Makefile
+++ Makefile
@@ -1,7 +1,19 @@
 
+# CC = gcc
+CFLAGS = -std=c99 -Wall -g
+LIBFLAGS = -lm
 
-CC=gcc
-CFLAGS=-std=c99 -Wall -g
+SRCS = mvfl.c linenoise.c mpc.c
+OBJS = $(SRCS:.c=.o)
+MAIN = mvfl
 
-mvfl: mvfl.c
-	$(CC) $(CFLAGS) mvfl.c linenoise.c -o mvfl
+$(MAIN): $(OBJS)
+	$(CC) $(CFLAGS) -o $(MAIN) $(OBJS) $(LIBFLAGS)
+
+# The following is an implicit rule, but I like it.
+# $< is the name of the related file that caused this action.
+.c.o:
+	$(CC) $(CFLAGS) -c $<
+
+clean:
+	rm *.o *.exe
--- mvfl.c
+++ mvfl.c
@@ -4,18 +4,47 @@
 #include <string.h>
 
 #include "linenoise.h"
+#include "mpc.h"
 
 static char* prompt = "mvfl> ";
 static char* prompt_exit = ":exit";
 
 int main( int argc, char** argv ) {
 
-    char* line;
+    mpc_parser_t* Zero       = mpc_new("zero");
+    mpc_parser_t* PosDigit   = mpc_new("posDigit");
+    mpc_parser_t* Digit      = mpc_new("digit");
+    mpc_parser_t* Digits     = mpc_new("digits");
+    mpc_parser_t* PosInteger = mpc_new("posInteger");
+    mpc_parser_t* NegInteger = mpc_new("negInteger");
+    mpc_parser_t* Integer    = mpc_new("integer");
+    mpc_parser_t* Float      = mpc_new("float");
+    mpc_parser_t* Number     = mpc_new("number");
+    mpc_parser_t* Zeros      = mpc_new("zeros");
+    mpc_parser_t* PaddedNumber = mpc_new("paddedNumber");
+    mpc_parser_t* Constant = mpc_new("constant");
+    mpc_parser_t* SignedConstant = mpc_new("signedConstant");
+
+    mpc_parser_t* Expression = mpc_new("expression");
+    mpc_parser_t* Term = mpc_new("term");
+    mpc_parser_t* Xponent = mpc_new("xponent");
+    mpc_parser_t* Factor = mpc_new("factor");
+
+    mpc_parser_t* Mvfl = mpc_new("mvfl");
+
+    mpca_lang_contents( MPCA_LANG_DEFAULT, "mvfl-grammar.gr",
+           Zero, PosDigit, Digit, Digits,
+           PosInteger, NegInteger, Integer, Float, Number, Zeros, PaddedNumber, Constant, SignedConstant,
+           Expression, Term, Xponent, Factor,
+           Mvfl );
+
+
+    /* Finish initializing grammar. */
 
     printf( "My Very First Language.\n" );
     printf( "Enter command `:exit` to exit.\n\n" );
 
-    /* In a never ending loop */
+    char* line;
     while ( ( line = linenoise( prompt ) ) != NULL ) {
 
         if ( strcmp( line, prompt_exit ) == 0 ) {
@@ -26,13 +55,26 @@ int main( int argc, char** argv ) {
 
         }
 
-        printf( "echo: %s\n", line );
-
         linenoiseHistoryAdd( line );
+
+        mpc_result_t parsed;
+        if ( mpc_parse("<stdin>", line, Mvfl, &parsed) ) {
+            mpc_ast_print( parsed.output );
+            mpc_ast_delete( parsed.output );
+        }
+        else {
+            mpc_err_print( parsed.error );
+            mpc_err_delete( parsed.error );
+        }
 
         free( line );
 
     }
+
+    mpc_cleanup( 18, Zero, PosDigit, Digit, Digits,
+                     PosInteger, NegInteger, Integer, Float, Number, Zeros, PaddedNumber, Constant, SignedConstant,
+                     Expression, Term, Xponent, Factor,
+                     Mvfl );
 
     return 0;
 
