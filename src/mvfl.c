bc4d43ee130d0e704d40e98337241d8e7151c250
Andrey Kaipov <akaip002@fiu.edu>
2016-04-02 23:06:34 -0400

    Add basic error handling

--- src/mvfl.c
+++ src/mvfl.c
@@ -11,58 +11,132 @@
 static char* prompt = "mvfl> ";
 static char* prompt_exit = ":exit";
 
-int number_of_nodes( mpc_ast_t* ast )
-{
+typedef struct {
+    int type;
+    long num;
+    int err;
+} mvfl_val;
 
-    if ( ast->children_num == 0 ) {
+enum { MVFL_NUM, MVFL_ERR };
+enum { MVFL_ERR_DIV_ZERO, MVFL_ERR_BAD_OP, MVFL_ERR_BAD_NUM };
 
-        return 1;
+mvfl_val mvfl_val_num( long num ) {
 
-    }
-    else {
+    mvfl_val value;
+    value.type = MVFL_NUM;
+    value.num = num;
 
-        int i = 0;
-        int totalNodes = 1;
+    return value;
 
-        for ( i = 0; i < ast->children_num; i++ ) {
+}
 
-            totalNodes += number_of_nodes( ast->children[ i ] );
+mvfl_val mvfl_val_err( int err ) {
 
-        }
+    mvfl_val value;
+    value.type = MVFL_ERR;
+    value.err = err;
 
-        return totalNodes;
+    return value;
+
+}
+
+void mvfl_val_print_num( mvfl_val value ) {
+
+    printf( "%li", value.num );
+
+}
+
+void mvfl_val_print_err( mvfl_val value ) {
+
+    switch ( value.err ) {
+
+        case MVFL_ERR_DIV_ZERO:
+            printf( "Error: Cannot divide by zero!" );
+            break;
+        case MVFL_ERR_BAD_OP:
+            printf( "Error: Invalid operator!" );
+            break;
+        case MVFL_ERR_BAD_NUM:
+            printf( "Error: Invalid number!" );
+            break;
+        default:
+            printf( "Oh there's some other kind of error." );
 
     }
 
 }
 
-int eval_op( double x, char* op, double y ) {
+void mvfl_val_print( mvfl_val value ) {
 
-    if ( strcmp(op,"+") == 0 || strcmp(op,"add") == 0 ) { return x + y; }
-    if ( strcmp(op,"-") == 0 || strcmp(op,"sub") == 0 ) { return x - y; }
-    if ( strcmp(op,"*") == 0 || strcmp(op,"mul") == 0 ) { return x * y; }
-    if ( strcmp(op,"/") == 0 || strcmp(op,"div") == 0 ) { return x / y; }
-    if ( strcmp(op,"min") == 0 ) { return x < y ? x : y; }
-    if ( strcmp(op,"max") == 0 ) { return y < x ? x : y; }
+    switch ( value.type ) {
+
+        case MVFL_NUM:
+            mvfl_val_print_num( value );
+            break;
+
+        case MVFL_ERR:
+            mvfl_val_print_err( value );
+            break;
+
+    }
+
+}
+
+void mvfl_val_println( mvfl_val value ) {
+
+    mvfl_val_print( value );
+    putchar( '\n' );
+
+}
+
+mvfl_val eval_op( mvfl_val x, char* op, mvfl_val y ) {
+
+    if ( x.type == MVFL_ERR ) return x;
+    if ( y.type == MVFL_ERR ) return y;
+    if ( strcmp(op,"+") == 0 || strcmp(op,"add") == 0 ) {
+        return mvfl_val_num( x.num + y.num );
+    }
+    else if ( strcmp(op,"-") == 0 || strcmp(op,"sub") == 0 ) {
+        return mvfl_val_num( x.num - y.num );
+    }
+    else if ( strcmp(op,"*") == 0 || strcmp(op,"mul") == 0 ) {
+        return mvfl_val_num( x.num * y.num );
+    }
+    else if ( strcmp(op,"/") == 0 || strcmp(op,"div") == 0 ) {
+        return y.num == 0 ? mvfl_val_err( MVFL_ERR_DIV_ZERO )
+                          : mvfl_val_num( x.num / y.num );
+    }
+    else if ( strcmp(op,"min") == 0 ) {
+        return x.num < y.num ? mvfl_val_num( x.num )
+                             : mvfl_val_num( y.num );
+    }
+    else if ( strcmp(op,"max") == 0 ) {
+        return y.num < x.num ? mvfl_val_num( x.num )
+                             : mvfl_val_num( y.num );
+    }
     // if ( strcmp(op, "%") == 0 ) { return x % y; }
-    if ( strcmp(op, "^") == 0 || strcmp(op,"pow") == 0 ) {
-        int total = 1;
+    else if ( strcmp(op, "^") == 0 || strcmp(op,"pow") == 0 ) {
+        long total = 1;
         int i;
-        for ( i = 0; i < y; i++ ) total *= x;
-        return total;
+        for ( i = 0; i < y.num ; i++ ) total *= x.num;
+        return mvfl_val_num( total );
     }
-    return 0;
+    return mvfl_val_num( 1338 );
 
 }
 
-int eval_arithmetic_expr( mpc_ast_t* ast )
-{
+mvfl_val eval_arithmetic_expr( mpc_ast_t* ast ) {
 
     // If tag contains integer, get the value of its contents.
     if ( strstr( ast->tag, "number" ) ) {
 
-        if ( strstr( ast->tag, "integer" ) ) { return atoi( ast->contents ); }
-        else return 1337;
+        if ( strstr( ast->tag, "integer" ) ) {
+            errno = 0;
+            long contents = strtol( ast->contents, NULL, 10 );
+            return errno != ERANGE ? mvfl_val_num( contents )
+                                   : mvfl_val_err( MVFL_ERR_BAD_NUM );
+        }
+        else return mvfl_val_num( 1337 );
 
     }
     // If root of tree, or if <Base> has children, take the middle one.
@@ -76,16 +150,17 @@ int eval_arithmetic_expr( mpc_ast_t* ast )
     else if ( strstr( ast->tag, "term" ) && ast->children_num == 2 ) {
 
         char* op = ast->children[ 0 ]->contents;
-        long x = eval_arithmetic_expr( ast->children[ 1 ] );
+        mvfl_val x = eval_arithmetic_expr( ast->children[ 1 ] );
+        mvfl_val zero = mvfl_val_num( 0 );
 
-        return eval_op( 0, op, x );
+        return eval_op( zero, op, x );
 
     }
     // Check prefix thing.
     else if ( strstr( ast->tag, "prefixExpression" ) ) {
 
         char* op = ast->children[ 0 ]->contents;
-        long x = eval_arithmetic_expr( ast->children[ 1 ] );
+        mvfl_val x = eval_arithmetic_expr( ast->children[ 1 ] );
 
         int i;
         for ( i = 2; i < ast->children_num; i += 1 ) {
@@ -100,7 +175,7 @@ int eval_arithmetic_expr( mpc_ast_t* ast )
     // Otherwise, just an expression or an infix expression. Loop from left to right, looking at each operator.
     else {
 
-        long x = eval_arithmetic_expr( ast->children[ 0 ] );
+        mvfl_val x = eval_arithmetic_expr( ast->children[ 0 ] );
 
         int i;
         for ( i = 1; i < ast->children_num; i += 2 ) {
@@ -157,9 +232,8 @@ int main( int argc, char** argv ) {
         mpc_result_t parsed;
         if ( mpc_parse( "<stdin>", line, Mvfl, &parsed ) ) {
 
-            long result = eval_arithmetic_expr( parsed.output );
-
-            printf("%li\n", result);
+            mvfl_val result = eval_arithmetic_expr( parsed.output );
+            mvfl_val_println( result );
 
             printf("Parse tree:\n");
             mpc_ast_print( parsed.output );
